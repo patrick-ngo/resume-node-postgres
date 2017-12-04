459f0e1801a70a3ed130938ba93900d516acc036
Patrick Ngo <patrick.ngo@hotmail.com>
2017-12-04 21:26:06 +0800

    added comments for routes index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,15 +6,22 @@ module.exports = (app) => {
         message: 'Welcome te Patrick Ngo Resume API!',
     }));
 
+    //create a resume
     app.post('/api/resumes', resumesController.create);
+    //list all resumes
     app.get('/api/resumes', resumesController.list);
-
+    //update a resume by id
     app.put('/api/resumes/:resumeId', resumesController.update);
+    //get resume by id
     app.get('/api/resumes/:resumeId', resumesController.retrieve);
+    //delete a resume by id
     app.delete('/api/resumes/:resumeId', resumesController.destroy);
-
+    
+    //create a job for resume id
     app.post('/api/resumes/:resumeId/jobs', jobsController.create);
+    //update a job for a resume by job id and resume id
     app.put('/api/resumes/:resumeId/jobs/:jobId', jobsController.update);
+    //delete a job for a reusume by job id and resume id
     app.delete('/api/resumes/:resumeId/jobs/:jobId', jobsController.destroy);
 
     // For any other request method on todo items, we're going to return "Method Not Allowed"
