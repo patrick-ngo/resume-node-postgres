2899ef1db89efa729f1f563bffe4bf25a824f341
Patrick Ngo <patrick.ngo@hotmail.com>
2017-12-04 21:28:33 +0800

    added comments for models job.js and resume.js

--- server/models/job.js
+++ server/models/job.js
@@ -1,12 +1,26 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Job = sequelize.define('Job', {
+
+    //job title
     title: DataTypes.STRING,
+
+    //company name
     company: DataTypes.STRING,
+
+    //period of employment
     period: DataTypes.STRING,
+
+    //industry type
     industry: DataTypes.STRING,
+
+    //location
     location: DataTypes.STRING,
+
+    //description of the job
     description: DataTypes.STRING,
+
+    //if the job is current or not
     current: DataTypes.BOOLEAN
   }, {
     classMethods: {
--- server/models/resume.js
+++ server/models/resume.js
@@ -1,15 +1,29 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Resume = sequelize.define('Resume', {
+
+    //name of the person
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    //profile image  
     profileImage: DataTypes.STRING,
+
+    //banner image
     bannerImage: DataTypes.STRING,
+
+    //title
     title: DataTypes.STRING,
+
+    //educational background
     education: DataTypes.STRING,
+
+    //location
     location: DataTypes.STRING,
+
+    //summary text
     summary: DataTypes.STRING,
   }, {
     classMethods: {
