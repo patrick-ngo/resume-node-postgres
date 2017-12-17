c9e4b5655540bf403c02d1660a15e5b288a3d215
Patrick Ngo <patrick.ngo@hotmail.com>
2017-12-17 22:56:50 +0800

    added migration script for creation of Educations table

---
+++ server/migrations/20171217144504-create-education.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Education', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      school: {
+        type: Sequelize.STRING
+      },
+      period: {
+        type: Sequelize.STRING
+      },
+      start: {
+        type: Sequelize.STRING
+      },
+      end: {
+        type: Sequelize.STRING
+      },
+      degree: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      current: {
+        type: Sequelize.BOOLEAN
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      resumeId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Education');
+  }
+};
